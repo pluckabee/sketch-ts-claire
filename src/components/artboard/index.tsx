7d045de40fc9992e3f06595ac6e0595df2ef6ed9
Claire Patterson <claire.patterson@gmail.com>
2021-07-20 16:35:40 +0100

    remove todo

--- src/components/artboard/index.tsx
+++ src/components/artboard/index.tsx
@@ -1,17 +1,19 @@
-import React from 'react';
-import { SketchArtboard } from  "../../typeInterfaces"
+import React from "react";
+import { SketchArtboard } from "../../typeInterfaces";
 
 type ArtboardViewProps = {
-    artboard: SketchArtboard
-}
-// TODO use router to navigate artboards
+  artboard: SketchArtboard;
+};
 
-const ArtboardView: React.FC<ArtboardViewProps> = ({ artboard}) => {
+const ArtboardView: React.FC<ArtboardViewProps> = ({ artboard }) => {
   return (
-      <main>
-        <img src={artboard.file.url} alt={`artboard file ${artboard.artboardName}`}></img>
+    <main>
+      <img
+        src={artboard.file.url}
+        alt={`artboard file ${artboard.artboardName}`}
+      ></img>
     </main>
   );
-}
+};
 
 export default ArtboardView;
