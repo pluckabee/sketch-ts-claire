68e51f8f31ceebeb267345ea8ffb84ec65142c92
Claire Patterson <claire.patterson@gmail.com>
2021-07-22 19:09:54 +0100

    adjust transition

--- src/components/_styled/thumbnailText.tsx
+++ src/components/_styled/thumbnailText.tsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 const ThumbnailText = styled.div`
   align-self: end;
-  transition: font-weight 0.3s ease-in-out;
+  transition: font-weight 0.2s ease-in-out;
   padding-bottom: 1rem;
 `;
 
